340823947755b0d1c08885a19615a35840dba372
Alvaro Ortiz Pedroso <alvaro.ortiz.pedroso@gmail.com>
2023-01-31 09:56:21 +0100

    proyecto de prueba de ApiRest

---
+++ src/app/clientes/cliente json.ts
@@ -0,0 +1,5 @@
+import {Cliente} from './cliente';
+export const CLIENTES: Cliente[]=[
+    {id: 1, nombre: 'Alvaro', apellido: 'Ortiz', email: 'algo@as.com', createAt: '2017-12-11'},
+    {id: 2, nombre: 'Paco', apellido: 'Perez', email: 'algo@as.com', createAt: '2017-12-11'}
+  ]
